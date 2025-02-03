afd7bf40e949ff422777e645b10c6e8ca2a55c0c
hrid1 <hridoycse1@gmail.com>
2025-02-04 01:52:50 +0600

    Upload file server Done

---
+++ class-6/file-upload/controllers/fileUpload.js
@@ -0,0 +1,25 @@
+const File = require("../models/File");
+
+const localFileUpload = async (req, res) => {
+  try {
+    // fetch file
+    const file = req.files.file;
+    console.log("Got the file: ", file);
+
+    let path =
+      __dirname + "/files/" + Date.now() + `.${file.name.split(".")[1]}`;
+
+    file.mv(path, (err) => {
+      console.log(err);
+    });
+
+    res.json({
+      success: true,
+      message: "Local file uploaded Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { localFileUpload };
--- class-6/file-upload/index.js
+++ class-6/file-upload/index.js
@@ -2,12 +2,23 @@ const express = require("express");
 const ConnectDB = require("./config/db");
 require("dotenv").config();
 const app = express();
-
+const fileUpload = require("express-fileupload");
+const cloudinaryConnect = require("./config/cloudinary");
+const Upload = require("./routes/FileUpload");
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json());
+app.use(fileUpload());
 
 // connect db
 ConnectDB();
+// connect cloud
+cloudinaryConnect();
+
+// api routes
+app.use("/api/v1/upload", Upload);
+
 app.get("/", (req, res) => {
   res.send("Hello File System!");
 });
